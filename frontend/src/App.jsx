6085e4dec321f8a90fbc829dddb12601a0726bb9
martina <martina.n.bikova.2020@elsys-bg.org>
2025-01-02 12:51:02 +0200

    started frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,175 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div class="grid grid-cols-5 gap-4">
+        <div class="class-block">
+          <div class="class">
+            <input name="subjectName" readOnly={true}></input>
+          </div>
+          <div class="class">
+          <input name="subjectName" readOnly={true}></input>
+          </div>
+        </div>
+        <div class="class-block">
+          <div class="class">
+            <input name="subjectName" readOnly={true}></input>
+          </div>
+          <div class="class">
+          <input name="subjectName" readOnly={true}></input>
+          </div>
+        </div>
+        <div class="class-block">
+          <div class="class">
+            <input name="subjectName" readOnly={true}></input>
+          </div>
+          <div class="class">
+          <input name="subjectName" readOnly={true}></input>
+          </div>
+        </div>
+        <div class="class-block">
+          <div class="class">
+            <input name="subjectName" readOnly={true}></input>
+          </div>
+          <div class="class">
+          <input name="subjectName" readOnly={true}></input>
+          </div>
+        </div>
+        <div class="class-block">
+          <div class="class">
+            <input name="subjectName" readOnly={true}></input>
+          </div>
+          <div class="class">
+          <input name="subjectName" readOnly={true}></input>
+          </div>
+        </div>
+        <div class="class-block">
+          <div class="class">
+            <input name="subjectName" readOnly={true}></input>
+          </div>
+          <div class="class">
+          <input name="subjectName" readOnly={true}></input>
+          </div>
+        </div>
+        <div class="class-block">
+          <div class="class">
+            <input name="subjectName" readOnly={true}></input>
+          </div>
+          <div class="class">
+          <input name="subjectName" readOnly={true}></input>
+          </div>
+        </div>
+        <div class="class-block">
+          <div class="class">
+            <input name="subjectName" readOnly={true}></input>
+          </div>
+          <div class="class">
+          <input name="subjectName" readOnly={true}></input>
+          </div>
+        </div>
+        <div class="class-block">
+          <div class="class">
+            <input name="subjectName" readOnly={true}></input>
+          </div>
+          <div class="class">
+          <input name="subjectName" readOnly={true}></input>
+          </div>
+        </div>
+        <div class="class-block">
+          <div class="class">
+            <input name="subjectName" readOnly={true}></input>
+          </div>
+          <div class="class">
+          <input name="subjectName" readOnly={true}></input>
+          </div>
+        </div>
+        <div class="class-block">
+          <div class="class">
+            <input name="subjectName" readOnly={true}></input>
+          </div>
+          <div class="class">
+          <input name="subjectName" readOnly={true}></input>
+          </div>
+        </div>
+        <div class="class-block">
+          <div class="class">
+            <input name="subjectName" readOnly={true}></input>
+          </div>
+          <div class="class">
+          <input name="subjectName" readOnly={true}></input>
+          </div>
+        </div>
+        <div class="class-block">
+          <div class="class">
+            <input name="subjectName" readOnly={true}></input>
+          </div>
+          <div class="class">
+          <input name="subjectName" readOnly={true}></input>
+          </div>
+        </div>
+        <div class="class-block">
+          <div class="class">
+            <input name="subjectName" readOnly={true}></input>
+          </div>
+          <div class="class">
+          <input name="subjectName" readOnly={true}></input>
+          </div>
+        </div>
+        <div class="class-block">
+          <div class="class">
+            <input name="subjectName" readOnly={true}></input>
+          </div>
+          <div class="class">
+          <input name="subjectName" readOnly={true}></input>
+          </div>
+        </div>
+        <div class="class-block">
+          <div class="class">
+            <input name="subjectName" readOnly={true}></input>
+          </div>
+          <div class="class">
+          <input name="subjectName" readOnly={true}></input>
+          </div>
+        </div>
+        <div class="class-block">
+          <div class="class">
+            <input name="subjectName" readOnly={true}></input>
+          </div>
+          <div class="class">
+          <input name="subjectName" readOnly={true}></input>
+          </div>
+        </div>
+        <div class="class-block">
+          <div class="class">
+            <input name="subjectName" readOnly={true}></input>
+          </div>
+          <div class="class">
+          <input name="subjectName" readOnly={true}></input>
+          </div>
+        </div>
+        <div class="class-block">
+          <div class="class">
+            <input name="subjectName" readOnly={true}></input>
+          </div>
+          <div class="class">
+          <input name="subjectName" readOnly={true}></input>
+          </div>
+        </div>
+        <div class="class-block">
+          <div class="class">
+            <input name="subjectName" readOnly={true}></input>
+          </div>
+          <div class="class">
+          <input name="subjectName" readOnly={true}></input>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
