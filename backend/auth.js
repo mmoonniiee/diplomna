23ab731a4a41c3b5b7f710f3f0f112eaf1587573
martina <martina.n.bikova.2020@elsys-bg.org>
2025-01-15 20:19:55 +0200

    passport google oauth

--- backend/auth.js
+++ backend/auth.js
@@ -1,21 +1,27 @@
 import passport from 'passport';
+import { findAndCreate } from './db';
 
 import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
 
-const GOOGLE_CLIENT_ID = `240896014026-79fd82ku0sgjmrn6921pmlvvgkbbkv1i.apps.googleusercontent.com`;
-const GOOGLE_CLIENT_SECRET = `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`;
-
-//TODO: ENVIROMENT VARIABLES!!!!!
-
 passport.use(new GoogleStrategy({
-    clientID: GOOGLE_CLIENT_ID,
-    clientSecret: GOOGLE_CLIENT_SECRET,
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     callbackURL: "http://localhost:5000/google/callback"
   },
   function(accessToken, refreshToken, profile, cb) {
-    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+    findAndCreate(profile.id, profile.displayName, profile.emails[0].value, function (err, user) {
       return cb(err, user);
     });
-    //todo??????
+    //todo
   }
 ));
+
+passport.serializeUser(function (user, done) {
+    done(null, user);
+    //todo
+});
+
+passport.deserializeUser(function (user, done) {
+    done(null, user);
+    //todo
+});
