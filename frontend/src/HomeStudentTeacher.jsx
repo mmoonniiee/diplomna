bbdb2dfbe23dd4cfca1220df82bf2c11c894f44f
martina <martina.n.bikova.2020@elsys-bg.org>
2025-01-25 16:44:02 +0200

    home pages for diff users

---
+++ frontend/src/HomeStudentTeacher.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './Header.jsx'
+
+export default function HomeST() {
+    return (
+        <div>
+            <Header />
+            <div>
+                <h1>Програма</h1>
+                <p>Проверете личното ви седмично разписание</p>
+            </div>
+        </div>
+    )
+}
---
+++ frontend/src/HomeTeacherAdmin.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from './Header.jsx';
+
+export default function HomeAT() {
+    return (
+        <div>
+            <div>
+                <h1>Редактиране на учители/ученици</h1>
+            </div>
+            <div>
+                <h1>Редактиране на седмично разписание</h1>
+            </div>
+            <div>
+                <h1>Преглед на личното Ви седмично разписание</h1>
+            </div>
+        </div>
+    )
+}
