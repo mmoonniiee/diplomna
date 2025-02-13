21adb84a9a1b61c2cb6c92cb5bd834d9d1e5151b
martina <martina.n.bikova.2020@elsys-bg.org>
2025-02-13 18:17:33 +0200

    about to rewrite the page

---
+++ frontend/src/ScheduleEditor.jsx
@@ -0,0 +1,10 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Tab, TabGroup, TabList, TabPanel, TabPanels } from '@headlessui/react'
+import Header from './components/Header';
+import { useQuery, useMutation }from 'react-query'
+import axios from 'axios';
+
+export default function ScheduleEditor(props) {
+    
+}
