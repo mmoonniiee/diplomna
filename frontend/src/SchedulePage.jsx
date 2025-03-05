59f7245ceb0380a0d720bafc016d1afad40c8d10
martina <martina.n.bikova.2020@elsys-bg.org>
2025-03-05 09:27:21 +0200

    schedule code

--- frontend/src/SchedulePage.jsx
+++ frontend/src/SchedulePage.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import Header from './components/Header';
 
 export default function SchedulePage() {
     return (
         <div>
-            <Header />
             <Table /> 
         </div>
     )
