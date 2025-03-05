089d9bea42d12e87e7482fc60e47c6e8d659fe2f
martina <martina.n.bikova.2020@elsys-bg.org>
2025-03-05 05:32:57 +0200

    added frontend routing

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,13 +1,21 @@
-import { route } from "@react-router/dev/routes";
+import { route, prefix, layout } from "@react-router/dev/routes";
   
   export default [
-    // * matches all URLs, the ? makes it optional so it will match / as well
-    route("*?", "catchall.jsx"),
-    route("/home/admin/teacher", "HomeTeacherAdmin.jsx"),
-    route("/home/admin", "HomeAdmin.jsx"),
-    route("/home", "HomeStudentTeacher.jsx"),
-    route("/admin/schedule", "schedule.jsx"),
-    route("/schedule", "SchedulePage.jsx"),
-    route("/admin/subjects", "SubjectEditor.jsx"),
+    ...prefix("/school/:schoolId", [
+      route("/home/admin/teacher", "HomeTeacherAdmin.jsx"),
+      route("/home/admin", "HomeAdmin.jsx"),
+      route("/home", "HomeStudentTeacher.jsx"),
+      route("/admin/term/:term", "./ScheduleTablist.jsx", [
+        route("/grade/:gradeId", "ScheduleEditorGrade.jsx"),
+        route("/teacher/:teacherId", "ScheduleEditorTeacher.jsx")
+      ]),
+      route("/schedule", "SchedulePage.jsx"),
+      layout("./components/Sidebar.jsx", [
+        route("/admin/subjects/add", "SubjectAdd.jsx"),
+        route("/admin/subjects/:subjectId/edit", "SubjectEditor.jsx")
+      ]),
+      route("/admin/grades/add", "GradeAdd.jsx")
+    ]),
+      route("/", "Start.jsx")
   ] 
   
