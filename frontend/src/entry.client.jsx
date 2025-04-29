446dcf69a1d0bd4e100e0ef221366063b72dc695
martina <martina.n.bikova.2020@elsys-bg.org>
2025-04-29 15:54:15 +0300

    imported correct css file

--- frontend/src/entry.client.jsx
+++ frontend/src/entry.client.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { HydratedRouter } from "react-router/dom";
-import "./index.css";
+import "./App.css";
 
 ReactDOM.hydrateRoot(
   document,
