fe4fb5621f9bc7381a031bb82a0daec15c566a97
martina <martina.n.bikova.2020@elsys-bg.org>
2025-04-29 18:52:32 +0300

    almost complete schedule render

--- frontend/src/SchedulePage.jsx
+++ frontend/src/SchedulePage.jsx
@@ -18,35 +18,88 @@ const idFromDay = (day) => {
   }
 }
 
+const dayFromId = (id) => {
+    switch(id) {
+        case 0: return "Понеделник";
+        case 1: return "Вторник";
+        case 2: return "Сряда";
+        case 3: return "Четвъртък";
+        case 4: return "Петък";
+        default: return "";
+    }
+}
+
 export default function SchedulePage({ loaderData }) {
     const schedule = loaderData;
 
     return (
         <div className='flex justify-center items-center h-screen w-screen'>
-            <div className='p-4 grid grid-cols-5 grid-rows-8 gap-4 bg-[rgba(217,217,217,0.4)] max-w-[70vw] max-h-[85vh] rounded-xl'>
-                {Array.from({length : 40}, (_, i) => {
-                    const cls = schedule.filter(obj => 
-                        (obj.class_number - 1) * 5 + idFromDay(obj.weekday_taught) === i);
-                    
+            <div className='p-4 grid grid-cols-5 gap-4 bg-[rgba(253,253,253,0.2)] max-w-[70vw] max-h-[85vh] rounded-xl'>
+                {Array.from({length: 5}, (_, day) => {
                     return (
-                        <div className='bg-[rgba(217,217,217,0.4)] max-w-[20vw] aspect-[3/1] rounded-full flex items-center overflow-hidden'>
-                            {cls.length > 1 ? (
-                                (() => {
-                                    const odd = cls.find(obj => obj.week_taught === 'odd');
-                                    const even = cls.find(obj => obj.week_taught === 'even');
-
+                        <div className='grid grid-rows-2 gap-2 grid-rows-[7%_93%]'>
+                            <div>
+                                <p className='text-white text-center'>{dayFromId(day)}</p>
+                            </div>
+                            <div className='grid grid-rows-4 gap-4'>
+                                {Array.from({length: 4}, (_, block) => {
                                     return (
-                                        <div className="flex justify-center items-center grid grid-cols-2 gap-2">
-                                            <div className='h-full py-0 px-1 justify-center bg-[rgba(217,217,217,0.4)] rounded'>
-                                                <p className='text-white flex items-center pl-[10px] overflow-hidden text-[10px]'>{odd.subject_name}</p>
-                                            </div>
-                                            <div className='py-0 px-1 bg-[rgba(217,217,217,0.4)] rounded'>
-                                                <p className='text-white flex items-center pl-[10px] overflow-hidden text-[10px]'>{even.subject_name}</p>
-                                            </div>
+                                        <div className='grid grid-rows-2 gap-2 bg-[rgba(253,253,253,0.2)] rounded-lg p-2'>
+                                            {Array.from({length: 2}, (_, cls) => {
+                                                const index = block * 2 + (cls % 2);
+                                                return (
+                                                    <div key={ index } className='rounded text-white'>
+                                                        {(() => {
+                                                            const classes = schedule.filter(obj => 
+                                                                obj.class_number === (index + 1) && idFromDay(obj.weekday_taught) === day);
+                                                            
+                                                            return classes.length > 1 ? 
+                                                            <div>
+                                                                {(() => {
+                                                                    const odd = classes.find(obj => obj.week_taught === 'odd');
+                                                                    const even = classes.find(obj => obj.week_taught === 'even');
+                                                                    
+                                                                    return (
+                                                                        <div className='flex justify-center items-center grid grid-cols-2 gap-2'>
+                                                                            <div className='bg-[rgba(253,253,253,0.2)] h-full w-full  rounded'>
+                                                                                <p className='overflow-hidden text-[10px]'>{odd.subject_name}</p>
+                                                                            </div>
+                                                                            <div className='bg-[rgba(253,253,253,0.2)] h-full w-full  rounded'>
+                                                                                <p className='overflow-hidden text-[10px]'>{even.subject_name}</p>
+                                                                            </div>
+                                                                        </div>
+                                                                    )
+                                                                })()}
+                                                            </div> : 
+                                                            classes.length === 1 ? (() => {
+                                                                return classes[0].week_taught === 'odd' ? 
+                                                                <div className='grid grid-cols-2'>
+                                                                    <div className='bg-[rgba(253,253,253,0.2)] h-full w-full rounded'>
+                                                                        <p className='overflow-hidden text-[12px]'>{classes[0]?.subject_name}</p>
+                                                                    </div>
+                                                                    <div></div>
+                                                                </div> : 
+                                                                classes[0].week_taught === 'even' ? 
+                                                                <div className='grid grid-cols-2'>
+                                                                    <div></div>
+                                                                    <div className='bg-[rgba(253,253,253,0.2)] h-full w-full  rounded'>
+                                                                        <p className='overflow-hidden text-[12px]'>{classes[0]?.subject_name}</p>
+                                                                    </div>
+                                                                </div> :
+                                                                <div className='bg-[rgba(253,253,253,0.2)] h-full w-full rounded'>
+                                                                    <p className='pl-1 overflow-hidden text-[12px]'>{classes[0]?.subject_name}</p>
+                                                                </div> 
+                                                            })() :
+                                                            <div className='bg-[rgba(253,253,253,0.2)] h-full w-full rounded'>
+                                                            </div>
+                                                        })()}
+                                                    </div>
+                                                )
+                                            })}
                                         </div>
-                                    );
-                                })()
-                            ) : <p className='text-white flex items-center pl-[10px] text-[12px]'>{cls[0]?.subject_name}</p> }
+                                    )
+                                })}
+                            </div>
                         </div>
                     )
                 })}
